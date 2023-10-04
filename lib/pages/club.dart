db33036355cd2192a488dee8b365d1cf375c2ea5
PaulBailey-1 <paul.ryan.bailey@gmail.com>
2023-10-03 23:48:52 -0500

    UI design, added join, profile, and club pages

---
+++ lib/pages/club.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/material.dart';
+
+class ClubPage extends StatefulWidget {
+  const ClubPage({super.key});
+
+  @override
+  State<ClubPage> createState() => _ClubPageState();
+}
+
+class _ClubPageState extends State<ClubPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Coding Club'),
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(16),
+        
+      ),
+    );
+  }
+}
---
+++ lib/pages/join.dart
@@ -0,0 +1,66 @@
+
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import 'profile.dart';
+
+class JoinPage extends StatefulWidget {
+  const JoinPage({super.key});
+
+  @override
+  State<JoinPage> createState() => _JoinPageState();
+}
+
+class _JoinPageState extends State<JoinPage> {
+  final codeController = TextEditingController();
+
+  @override
+  void dispose() {
+    codeController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Join your Club'),
+      ),
+      body: Center(
+        child: Card(
+          margin: const EdgeInsets.symmetric(horizontal: 64),
+          child: Container(
+            margin: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text('Enter a join code', style: TextStyle(fontSize: 20)),
+                TextField(
+                  controller: codeController,
+                  textAlign: TextAlign.center,
+                  decoration: const InputDecoration(hintText: 'XXX-XXX'),
+                ),
+                const Padding(padding: EdgeInsets.symmetric(vertical: 8)),
+                ElevatedButton(
+                    onPressed: () {
+                      // checkJoinCode(codeController.text);
+                      if (true) {
+                        Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(
+                            builder: (context) => const ProfilePage(),
+                          ),
+                        );
+                      } else {
+                        Fluttertoast.showToast( msg: "Invalid Join Code", );
+                      }
+                    },
+                    child: const Text('Join', style: TextStyle(fontSize: 20))),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
